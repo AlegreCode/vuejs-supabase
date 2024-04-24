2d798fb324db890b8e3798ea41c70b977624592a
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-04-24 17:33:49 -0300

    Creamos el formulario, con el campo tittle

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <div class="row">
+      <div class="col-3 mt-5">
+        <div class="card">
+          <div class="card-body">
+            <h2 class="display-5 text-center">Subir Imágen</h2>
+            <form>
+              <div class="form-group mb-3">
+                <label for="title">Título</label>
+                <input type="text" class="form-control" id="title">
+              </div>
+              <div class="form-group d-grid">
+                <button type="submit" class="btn btn-primary">Subir</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>

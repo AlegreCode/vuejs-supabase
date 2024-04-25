256117fdd6e48f4a611d0c0c9c9666b9dea740b6
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-04-25 16:40:29 -0300

    Importamos el componente Gallery.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Form from './components/Form.vue';
+import Gallery from './components/Gallery.vue';
 </script>
 
 <template>
   <div class="container">
     <div class="row pt-5">
       <Form />
+      <Gallery />
     </div>
   </div>
 </template>

02dbb9f0383506b6945e720247729d8f158a8cbe
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-04-25 17:33:18 -0300

    Creamos el componente Gallery.vue

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="col-9">
         <h2 class="display-5 text-center p-3">Galería</h2>
+        <hr>
+        <div class="grid-container">
+            <img src="https://picsum.photos/600/300/" class="image">
+            <img src="https://picsum.photos/600/300/" class="image">
+            <img src="https://picsum.photos/600/300/" class="image">
+            <img src="https://picsum.photos/600/300/" class="image">
+            <img src="https://picsum.photos/600/300/" class="image">
+            <img src="https://picsum.photos/600/300/" class="image">
+            <img src="https://picsum.photos/600/300/" class="image">
+            <img src="https://picsum.photos/600/300/" class="image">
+            <img src="https://picsum.photos/600/300/" class="image">
+        </div>
     </div>
 </template>

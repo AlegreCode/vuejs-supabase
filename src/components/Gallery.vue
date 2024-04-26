baa3d6e11315570f48f33c4ef387891288e064f6
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-04-26 16:50:31 -0300

    Actualizamos el componente con elementos figure y figcaption

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -3,15 +3,22 @@
         <h2 class="display-5 text-center p-3">Galería</h2>
         <hr>
         <div class="grid-container">
-            <img src="https://picsum.photos/600/300/" class="image">
-            <img src="https://picsum.photos/600/300/" class="image">
-            <img src="https://picsum.photos/600/300/" class="image">
-            <img src="https://picsum.photos/600/300/" class="image">
-            <img src="https://picsum.photos/600/300/" class="image">
-            <img src="https://picsum.photos/600/300/" class="image">
-            <img src="https://picsum.photos/600/300/" class="image">
-            <img src="https://picsum.photos/600/300/" class="image">
-            <img src="https://picsum.photos/600/300/" class="image">
+            <figure class="box-image">
+                <img src="https://picsum.photos/600/300/" class="image">
+                <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
+            </figure>
+            <figure class="box-image">
+                <img src="https://picsum.photos/600/300/" class="image">
+                <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
+            </figure>
+            <figure class="box-image">
+                <img src="https://picsum.photos/600/300/" class="image">
+                <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
+            </figure>
+            <figure class="box-image">
+                <img src="https://picsum.photos/600/300/" class="image">
+                <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
+            </figure>
         </div>
     </div>
 </template>

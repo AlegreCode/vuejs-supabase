511ac852972d34092df9b0fd58a4111e487848be
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-04-27 16:51:42 -0300

    Creamos el componente BoxImage.vue

---
+++ src/components/BoxImage.vue
@@ -0,0 +1,6 @@
+<template>
+    <figure class="box-image">
+        <img src="https://picsum.photos/600/300/" class="image">
+        <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
+    </figure>
+</template>

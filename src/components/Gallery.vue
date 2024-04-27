12f34a3b4a46feb605d7f3f625fcb4a47233b85b
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-04-27 17:10:37 -0300

    Definimos el objeto data con los datos de las imágenes. Importamos el componente BoxImage.vue y le pasamos el objeto data.

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,24 +1,58 @@
+<script setup>
+import { ref } from 'vue'
+import BoxImage from './BoxImage.vue'
+
+const data = ref([
+  {
+    "title": "image1",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image2",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image3",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image4",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image5",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image6",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image7",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image8",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image9",
+    "url": "https://picsum.photos/600/300"
+  },
+  {
+    "title": "image10",
+    "url": "https://picsum.photos/600/300"
+  }
+])
+</script>
 <template>
     <div class="col-9">
         <h2 class="display-5 text-center p-3">Galería</h2>
         <hr>
         <div class="grid-container">
-            <figure class="box-image">
-                <img src="https://picsum.photos/600/300/" class="image">
-                <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
-            </figure>
-            <figure class="box-image">
-                <img src="https://picsum.photos/600/300/" class="image">
-                <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
-            </figure>
-            <figure class="box-image">
-                <img src="https://picsum.photos/600/300/" class="image">
-                <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
-            </figure>
-            <figure class="box-image">
-                <img src="https://picsum.photos/600/300/" class="image">
-                <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
-            </figure>
+            <template v-for="img in data">
+                <BoxImage :img="img"/>
+            </template>
         </div>
     </div>
 </template>

937e5344689d7bf94bc5eaa682183f01e3736d2b
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-04-27 17:09:40 -0300

    Actualizamos el componente BoxImage para que pueda recibir el objeto img y mostar los datos.

--- src/components/BoxImage.vue
+++ src/components/BoxImage.vue
@@ -1,6 +1,9 @@
+<script setup>
+const { img } = defineProps(['img'])
+</script>
 <template>
     <figure class="box-image">
-        <img src="https://picsum.photos/600/300/" class="image">
-        <figcaption class="title-image d-flex justify-content-center align-items-center display-5">title</figcaption>
+        <img :src="img.url" class="image">
+        <figcaption class="title-image d-flex justify-content-center align-items-center display-5">{{img.title}}</figcaption>
     </figure>
 </template>

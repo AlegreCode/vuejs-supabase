ca302e663bb88049c6b773d33412b6b8646bd953
Carlos Alberto Alegre <ca.alegre269@gmail.com>
2024-05-07 17:32:49 -0300

    Corregimos el campo url_image.

--- src/components/BoxImage.vue
+++ src/components/BoxImage.vue
@@ -1,9 +1,10 @@
 <script setup>
-const { img } = defineProps(['img'])
+
+const { img } = defineProps({img: { type: Object, required: true}});
 </script>
 <template>
     <figure class="box-image">
-        <img :src="img.url" class="image">
+        <img :src="img.url_image" class="image">
         <figcaption class="title-image d-flex justify-content-center align-items-center display-5">{{img.title}}</figcaption>
     </figure>
 </template>
